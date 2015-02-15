3ce7c3457720d6ae69660e53fe581e400cc39b05
Arjun Bhatnagar <me@arjunb.com>
2015-02-15 11:45:25 -0500

    added basic edge handling

--- receive.py
+++ receive.py
@@ -51,6 +51,8 @@ def receive(duration=1/1000,pin=23):
                 return pulses
             else:
                 if (am_reading):
+                    if (read_pulse[0] == 2 or read_pulse[0] == 6)
+                        read_pulse[0] += 1
                     pulses.append(read_pulse)
             count = 0
         else:
