ff9e1c93c432c5e5da3f2d714ec619524971b4af
Arjun Bhatnagar <me@arjunb.com>
2015-02-15 13:57:41 -0500

    next attempt at fixing spaces

--- datalinklayer.py
+++ datalinklayer.py
@@ -3,10 +3,9 @@ from constants import *
 from transmit import get
 
 def encode():
-	stack = MorseBJStack()
+    stack = MorseBJStack()
     encoded = stack.encode(message)
-	return encoded
-
+    return encoded
 
 def push(encoded_message):
     get(encoded_message)
@@ -15,8 +14,8 @@ def get(ip_protocol, payload, destination):
     dest = encode(destination)
     payload = encode(payload)
     ip_protocol = encode(ip_protocol)
-	src = encode(mac)
-	sep = [(1,1), (1,0), (3,1), (1,0), (1,1), (1,0), (3,1), (1,0)]
+    src = encode(mac)
+    sep = [(1,1), (1,0), (3,1), (1,0), (1,1), (1,0), (3,1), (1,0)]
     start = [(20,1),(1,0)]
     stop = [(40,1)]
-	push(start + src + sep + dest + sep + ip_protocol + sep + payload + stop)
+    push(start + src + sep + dest + sep + ip_protocol + sep + payload + stop)
