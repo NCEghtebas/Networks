5ab71f2b83a36dc67a6248998613c4eb8b1b6298
Arjun Bhatnagar <me@arjunb.com>
2015-03-05 17:05:26 -0500

    Made both sides of the stack parallel

--- datalinklayer.py
+++ datalinklayer.py
@@ -36,39 +36,50 @@ def decode_header(header, end_header_index):
     for item in header_items:
         d = decode(item)
         if count == 0:
-            decoded_header["SOURCE"] = d
+            decoded_header["SOURCE_LAN"] = d
         elif count == 1:
-            decoded_header["DESTINATION"] = d
+            decoded_header["SOURCE_HOST"] = d
         elif count == 2:
+            decoded_header["DESTINATION_LAN"] = d
+        elif count == 3:
+            decoded_header["DESTINATION_HOST"] = d
+        elif count == 4:
             decoded_header["IP_PROTOCOL"] = d
+        elif count == 5:
+            decoded_header["CHECKSUM"] = d
+        elif count == 6:
+            decoded_header["PAYLOAD"] = d
         count += 1
     return decoded_header
-        
+
 def decode_message(message):
     #Declare End Header Sequence
     end_header = [(3,1), (1,0), (1,1), (1,0), (1,1), (1,0), (1,1), (1,0), (3,1), (1,0)]
     end_header_indices = find_sub_list(end_header,message)
     end_header_indices = end_header_indices[0]
-    
+
     #Split message according to header and payload
     header = message[1:end_header_indices[1]+1]
     payload = message[end_header_indices[1]+1:len(message)-1]
-    
+
     #decode header
     decoded_message = decode_header(header, end_header_indices[0])
-    
+
     #check if packet is for me or someone else
     #if for someone else, do nothing or reroute packet
-    if (decoded_message["DESTINATION"] != mac):
-        return decoded_message["DESTINATION"]
+    if (decoded_message["DESTINATION_HOST"] != "1"): #CHANGE TO IP
+        return decoded_message["DESTINATION_HOST"]
 
     #Remove Source & Destination information from message
-    decoded_message.pop("SOURCE", None)
-    decoded_message.pop("DESTINATION", None)
+    decoded_message.pop("SOURCE_LAN", None)
+    decoded_message.pop("SOURCE_HOST", None)
+    decoded_message.pop("DESTINATION_LAN", None)
+    decoded_message.pop("DESTINATION_HOST", None)
+    decoded_message.pop("CHECKSUM", None)
 
     #decode payload
     decoded_message["PAYLOAD"] = decode(payload)
-    
+
     return decoded_message
 
 def push_up(decoded_message):
@@ -79,16 +90,26 @@ def push_down(encoded_message):
     from transmit import get_from_datalink_layer
     get_from_datalink_layer(encoded_message)
 
-def get_from_ip_layer(ip_protocol, payload, destination):
-    dest = encode(destination)
-    payload = encode(payload)
-    ip_protocol = encode(ip_protocol)
-    src = encode(mac)
+def get_from_ip_layer(message):
+    #encode the entire message
+    src_lan = "A"
+    src_host = '1' #should actually be pulled from router
+
+    message['SOURCE_LAN'] = encode(src_lan)
+    message['SOURCE_HOST'] = encode(src_host)
+    message["DESTINATION_LAN"] = encode(message["DESTINATION_LAN"])
+    message["DESTINATION_HOST"] = encode(message["DESTINATION_HOST"])
+    message["IP_PROTOCOL"] = encode(message["IP_PROTOCOL"])
+    message['CHECKSUM'] = encode("CHECK") #replace with checksum function
+    message["PAYLOAD"] = encode(message["PAYLOAD"])
+
+    #start|stop code for msg and header
     sep = [(1,1), (1,0), (3,1), (1,0), (1,1), (1,0), (3,1), (1,0)]
     end_header = [(3,1), (1,0), (1,1), (1,0), (1,1), (1,0), (1,1), (1,0), (3,1), (1,0)]
     start = [(20,1),(1,0)]
     stop = [(40,1)]
-    push_down(start + src + sep + dest + sep + ip_protocol + end_header + payload + stop)
+
+    push_down(start + message['SOURCE_LAN'] + sep + message['SOURCE_HOST'] + sep + message["DESTINATION_LAN"] + sep + message["DESTINATION_HOST"] + sep + message["IP_PROTOCOL"] + sep + message['CHECKSUM'] + end_header + message["PAYLOAD"] + stop)
 
 
 def get_from_physical_layer(message):
@@ -96,6 +117,6 @@ def get_from_physical_layer(message):
     decoded_len = len(decoded)
     mac_address_len = 1
     if decoded_len == mac_address_len:
-        print("REROUTE MESSAGE TO MAC ADDRESS: " + decoded)
+        print("REROUTE MESSAGE TO IP ADDRESS: " + decoded)
     else:
         push_up(decoded)
--- iplayer.py
+++ iplayer.py
@@ -1,6 +1,6 @@
-def push_down(ip_protocol, payload, destination):
+def push_down(message):
     from datalinklayer import get_from_ip_layer
-    get_from_ip_layer(ip_protocol, payload, destination)
+    get_from_ip_layer(encoded_message)
 
 def get_from_datalink_layer(decoded_message):
     protocol = decoded_message["IP_PROTOCOL"]
@@ -9,10 +9,20 @@ def get_from_datalink_layer(decoded_message):
     print(statement)
 
 def main():
-    destination = input('Recipient (A, B, or C): ')
+    destination = input('Destination: ')
     payload = input('Input Message: ')
-    ip_protocol = "Av0"
-    push_down(ip_protocol, payload, destination)
+    ip_protocol = "A"
+    while (len(destination) != 2):
+        print("ERROR: DESTINATION MUST BE 2 CHARS")
+        destination = input('Destination: ')
+
+    message = {}
+    message["PAYLOAD"] = payload
+    message["IP_PROTOCOL"] = ip_protocol
+    message["DESTINATION_LAN"] = destination[0]
+    message["DESTINATION_HOST"] = destination[1]
+
+    push_down(message)
 
 if __name__ == "__main__":
 	main()
